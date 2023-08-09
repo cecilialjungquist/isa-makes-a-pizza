6f9a25266369ad484d6d9da74ac6dc823f3d635b
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-08-09 17:58:01 +0200

    cleanup

--- src/views/StepSix.jsx
+++ src/views/StepSix.jsx
@@ -10,9 +10,9 @@ function StepSix() {
             <figure>
                 <img src="https://media.giphy.com/media/XzXjgGZOiTjF7d0JOp/giphy.gif" alt="" />
             </figure>
-            <p>You have now completed this guide for making the perfect Billy's pizza - congratulations! Now is the time to relax and enjoy your hard work.</p>
-            <p>If you wan't to eat your Billy's like Isa does, just skip the plate and eat with your hand! And if you REALLY want to get that Isa touch, add a nasty drink to it - for example a Monster energy drink!</p>
-            <p className="italic">Thank you for your participation - and a BIG thanks to Isa for being such an inspiring Billy's eater!</p>
+            <p>You have now completed this guide for making the perfect Billys pizza - congratulations! Now is the time to relax and enjoy your hard work.</p>
+            <p>If you wan't to eat your Billys like Isa does, just skip the plate and eat with your hand! And if you REALLY want to get that Isa touch, add a nasty drink to it - for example a Monster energy drink!</p>
+            <p className="italic">Thank you for your participation - and a BIG thanks to Isa for being such an inspiring Billys eater!</p>
             <p className="small italic">Click <Link to='/'>here</Link> if you want to restart this guide!</p>
         </section>
     );
