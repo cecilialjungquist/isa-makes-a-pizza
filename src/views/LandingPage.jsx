d716e6e674c7984f0af6813f5a938fef84bbf634
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-08-09 17:24:23 +0200

    added images

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,13 +1,25 @@
 import { useNavigate } from "react-router-dom";
+// import Isa from "../assets/isabilly.pdf";
 import Button from "../components/Button";
 
 function LandingPage() {
     const navigate = useNavigate();
+    let isa
     
     return (  
-        <section>
+        <section className="landing-page">
             <h1><span>Welcome to <br/></span>Isa Makes A Pizza!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti at voluptates asperiores fuga laudantium repellat odit illo soluta necessitatibus vel velit, in esse repudiandae corporis expedita accusantium consequatur ipsum, quam amet facilis maxime quod. Esse sapiente deserunt veritatis ex dignissimos!</p>
+
+            <figure>
+               <img src="/src/assets/isabilly.jpg" alt="" />
+            </figure>            
+
+            <p>Are you into driving different cars every week? Are those often of brands who has absolutely nothing interesting about them (for example Fiat or Ford)?</p>
+                <p>Are you starting 4/5 sentences with "I have no clue what that is."? </p>
+                <p>Do you hate super cute video games that is impossible to hate for everyone else (like Stardew Valley)?</p>
+                <p>Is Billy's pizza a common lunch for you, often heated in the microwave because you lack skills of both patience and planning (not unlike a 6 year old kid)?</p>
+
+            <p className="bold">Then you are just like our friend Isa, and this quide will help you make the best Billy's pizza in the world!</p>
             <Button handleClick={() => navigate('/step-one')}>Let's start!</Button>
         </section>
     );
--- src/views/StepSix.jsx
+++ src/views/StepSix.jsx
@@ -11,6 +11,9 @@ function StepSix() {
             <h3>STEP 6</h3>
             <h1>The Grand Finale</h1>
             <p className="bold">You are amazing!</p>
+            <figure>
+                <img src="https://media.giphy.com/media/XzXjgGZOiTjF7d0JOp/giphy.gif" alt="" />
+            </figure>
             <p>You have now completed this guide for making the perfect Billy's pizza - congratulations! Now is the time to relax and enjoy your hard work.</p>
             <p>If you wan't to eat your Billy's like Isa does, just skip the plate and eat with your hand! And if you REALLY want to get that Isa touch, add a nasty drink to it - for example a Monster energy drink!</p>
             <p className="italic">Thank you for your participation - and a BIG thanks to Isa for being such an inspiring Billy's eater!</p>
